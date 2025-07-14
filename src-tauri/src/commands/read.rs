a0f1facdc2f914e4fe44edfeb6c0d01a30d89e94
Marios <xrteach@gmail.com>
2025-07-14 16:59:18 +0300

    feat: add extension checking

--- src-tauri/src/commands/read.rs
+++ src-tauri/src/commands/read.rs
@@ -1,9 +1,20 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use base64::Engine;
 
 #[tauri::command]
 pub fn read_pdf_file(file: String) -> Result<String, ()> {
+    let path = PathBuf::from(file.clone());
+
+    let ext = path
+        .extension()
+        .and_then(|e| e.to_str())
+        .map(|e| e.eq_ignore_ascii_case("pdf"))
+        .unwrap_or(false);
+    if !ext {
+        return Err(());
+    }
+
     let file = match fs::read(file) {
         Ok(v) => v,
         Err(_) => return Err(()),
